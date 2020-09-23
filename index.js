ef7b61b7975f62a9c24b0556686ba2a3f48e6fea
Manikanth Reddy Abbu <53967637+ManikanthReddyAbbu@users.noreply.github.com>
2020-09-23 12:54:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send(' Hi there ! I am using KNative tools !! \n');
+  res.send(' Welcome to demo !! \n');
 });
 
 var server = app.listen(3000, function () {
